ae5004d5f10d827644eeebf0f918a9960b40adcf
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-03-17 03:38:58 +0800

    Update device.h
    Signed-off-by: kiddin9 <48883331+kiddin9@users.noreply.github.com>

--- device.h
+++ device.h
@@ -61,8 +61,6 @@ enum {
 	DEV_ATTR_ARP_ACCEPT,
 	DEV_ATTR_AUTH,
 	DEV_ATTR_AUTH_VLAN,
-	DEV_ATTR_SPEED,
-	DEV_ATTR_DUPLEX,
 	DEV_ATTR_VLAN,
 	DEV_ATTR_PAUSE,
 	DEV_ATTR_ASYM_PAUSE,
@@ -134,8 +132,6 @@ enum {
 	DEV_OPT_DROP_GRATUITOUS_ARP	= (1ULL << 27),
 	DEV_OPT_DROP_UNSOLICITED_NA	= (1ULL << 28),
 	DEV_OPT_ARP_ACCEPT		= (1ULL << 29),
-	DEV_OPT_SPEED			= (1ULL << 30),
-	DEV_OPT_DUPLEX			= (1ULL << 31),
 	DEV_OPT_PAUSE			= (1ULL << 32),
 	DEV_OPT_ASYM_PAUSE		= (1ULL << 33),
 	DEV_OPT_RXPAUSE			= (1ULL << 34),
@@ -219,8 +215,6 @@ struct device_settings {
 	bool drop_unsolicited_na;
 	bool arp_accept;
 	bool auth;
-	unsigned int speed;
-	bool duplex;
 	bool pause;
 	bool asym_pause;
 	bool rxpause;
